21adf75c45cc350bb1ffb8cb683a1226d404e048
markob52 <mb55652@fer.hr>
2025-11-11 17:05:34 +0100

    inicijalni commit

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-
+    char* line;
+    size_t len;
+    while (getline(&line,&len,stdin)!=-1) {
+        printf("<s>%s",line);
+        free(line);
+    }
     return EXIT_SUCCESS;
 }
